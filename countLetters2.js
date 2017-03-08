d171c9724f3043ab032d99ab0b4364d97d01d852
Lucas Brizolla <lbbrizolla@gmail.com>
2017-03-08 19:00:34 +0000

    add Character Position Exercise

--- countLetters2.js
+++ countLetters2.js
@@ -2,17 +2,13 @@ function countLetters(str) {
   var result = {};
   for (var i = 0; i < str.length; i++){
     if (result[str[i]]) {
-      result[str[i]] = result[str[i]] + 1;
+      result[str[i]].push(i);
     } else {
-      result[str[i]] = 1;
+      result[str[i]] = [i];
     }
   }
   delete result[' '];
   console.log(result);
 }
 
-
-
 countLetters('lighthouse in the house');
-
-
